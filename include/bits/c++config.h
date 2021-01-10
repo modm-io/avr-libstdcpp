fa1722e6802783b737e389e77dd41ff56110f1f1
Christopher Durand <christopher.durand@rwth-aachen.de>
2021-01-10 13:10:20 +0100

    Enable _GLIBCXX_USE_C99_MATH_TR1 to provide more math functions in std
    Not all will work because their in no implementation in avr-libm and
    will trigger linker errors when used ouside of a constant expression.

--- include/bits/c++config.h
+++ include/bits/c++config.h
@@ -1620,7 +1620,7 @@ namespace std
 
 /* Define if C99 functions or macros in <math.h> should be imported in
    <tr1/cmath> in namespace std::tr1. */
-//#define _GLIBCXX_USE_C99_MATH_TR1 1
+#define _GLIBCXX_USE_C99_MATH_TR1 1
 
 /* Define if C99 types in <stdint.h> should be imported in <tr1/cstdint> in
    namespace std::tr1. */
